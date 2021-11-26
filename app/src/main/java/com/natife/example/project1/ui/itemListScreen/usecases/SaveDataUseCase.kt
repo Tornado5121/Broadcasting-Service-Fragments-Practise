7e71b1d760e6d305455d3b764a2009f69b6e64f3
Tornado5121 <zhadkoroman@gmail.com>
2021-11-26 11:58:26 +0200

    optimising saveData method

--- app/src/main/java/com/natife/example/project1/ui/itemListScreen/usecases/SaveDataUseCase.kt
+++ app/src/main/java/com/natife/example/project1/ui/itemListScreen/usecases/SaveDataUseCase.kt
@@ -9,14 +9,15 @@ import com.natife.example.project1.ui.itemListScreen.MyItemsStates
 private val ID_KEY = "id"
 private val SHARED_PREF_FILE_NAME = "SharedPrefs"
 
-class SaveDataUseCase(val context: Context) : UseCase<MyItemsStates, MyItemsActions> {
+class SaveDataUseCase(context: Context) : UseCase<MyItemsStates, MyItemsActions> {
 
     private val sharedPref: SharedPreferences =
         context.getSharedPreferences(SHARED_PREF_FILE_NAME, Context.MODE_PRIVATE)
 
     override fun execute(state: MyItemsStates, action: MyItemsActions): MyItemsActions {
         return if (action is MyItemsActions.Save) {
-            MyItemsActions.DataSaved(saveData(action.id).getInt(ID_KEY, 1))
+            saveData(action.id)
+            MyItemsActions.DataSaved(sharedPref.getInt(ID_KEY,-1))
         } else {
             MyItemsActions.None
         }
@@ -26,10 +27,9 @@ class SaveDataUseCase(val context: Context) : UseCase<MyItemsStates, MyItemsActi
         return action is MyItemsActions.Save
     }
 
-    private fun saveData(id: Int): SharedPreferences {
+    private fun saveData(id: Int) {
         val editor = sharedPref.edit()
         editor.putInt(ID_KEY, id)
         editor.apply()
-        return sharedPref
     }
 }
